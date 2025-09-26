49b363517842ef910ecebef052d66d9673e42ffc
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-27 00:34:55 +0530

    Adding code for starting my first code in java 24

---
+++ src/FirstCodeOnJavaLatest.java
@@ -0,0 +1,7 @@
+public class FirstCodeOnJavaLatest {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java 24");
+        System.out.println("Refreshing my Java knowledge");
+        System.out.print("Lets see how long it takes for me to complete this course");
+    }
+}
