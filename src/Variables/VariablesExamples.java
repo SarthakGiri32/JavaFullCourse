8ebc1fd5f57e5d7ebb59b680aa08091f8d17e4ce
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-27 14:56:15 +0530

    Adding code for completing variable exercises

--- src/Variables/VariablesExamples.java
+++ src/Variables/VariablesExamples.java
@@ -20,21 +20,34 @@ public class VariablesExamples {
 //        boolean isStudent = true, isForSale = false, isOnline = true;
 
         // string
-        String fullName = "Judas Iscariot", money = "30 pieces of silver";
-        String emailId = "saved_sinner89662@godsaves.com", car = "Mercedes AMG Petronas Racing Formula 1";
+//        String fullName = "Judas Iscariot", money = "30 pieces of silver", color = "violet";
+//        String emailId = "saved_sinner89662@godsaves.com", car = "Mercedes AMG Petronas Racing Formula 1";
 //        System.out.println("Full name: " + fullName);
 //        System.out.println(fullName + " loves " + money);
 //        System.out.println("\nYour email is: " + emailId);
 //        System.out.println("Your favourite car is: " + car);
 
         // exercises
-        int age = 91;
-        double cgpa = 4.2;
-        char grade = 'E';
-        System.out.println(("Hello " + fullName));
-        System.out.println("You are " + age + " years old");
-        System.out.println("Your cgpa is: " + cgpa);
-        System.out.println("Your average letter grade is: " + grade);
+//        int age = 91;
+//        double cgpa = 4.2;
+//        char grade = 'E'; boolean isStudent = true;
+//        System.out.println(("Hello " + fullName));
+//        System.out.println("You are " + age + " years old");
+//        System.out.println("Your cgpa is: " + cgpa);
+//        System.out.println("Your average letter grade is: " + grade);
+
+        String color = "violet", car = "Mclaren P1"; boolean isForSale = true;
+        int year = 1915; double carPrice = 4999999.99; char currencySymbol = '₹';
+
+        System.out.println("Your choice is a " + color + " " + year + " " + car);
+        System.out.println("The price is: " + currencySymbol + carPrice);
+
+        if (isForSale) {
+            System.out.println("There is a " + car + " for sale");
+        }
+        else {
+            System.out.println("The " + car + " is not for sale");
+        }
 
     }
 }
