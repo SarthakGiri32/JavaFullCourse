2fdbcb9cc966e9daf7e37525cd053c9fed7811de
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-27 13:56:05 +0530

    Adding code for starting variables tutorial

---
+++ src/Variables/VariablesExamples.java
@@ -0,0 +1,21 @@
+package Variables;
+
+public class VariablesExamples {
+    public static void main(String[] args) {
+        // int
+//        int age = 91, year = 1915, productQuantity = 1000023;
+//        System.out.println(age);
+//        System.out.println("The year is: " + year);
+//        System.out.println("The product quantity is: " + productQuantity);
+
+        // double
+//        double productPrice = 20001.72, cgpa = 4.2, temperature = -75.81;
+//        System.out.println("₹" + productPrice);
+
+        // char
+        char grade = 'E', symbol = '^', currency = '₹';
+
+        System.out.println("Grade: " + grade + "\nSymbol: " + symbol + "\nCurrency: " + currency);
+
+    }
+}
