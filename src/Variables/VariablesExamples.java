e783c1ea5267650758f92a2a3e6a7123c024f8de
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-27 14:24:26 +0530

    Adding code for string examples and starting variable exercises

--- src/Variables/VariablesExamples.java
+++ src/Variables/VariablesExamples.java
@@ -20,7 +20,21 @@ public class VariablesExamples {
 //        boolean isStudent = true, isForSale = false, isOnline = true;
 
         // string
-        
+        String fullName = "Judas Iscariot", money = "30 pieces of silver";
+        String emailId = "saved_sinner89662@godsaves.com", car = "Mercedes AMG Petronas Racing Formula 1";
+//        System.out.println("Full name: " + fullName);
+//        System.out.println(fullName + " loves " + money);
+//        System.out.println("\nYour email is: " + emailId);
+//        System.out.println("Your favourite car is: " + car);
+
+        // exercises
+        int age = 91;
+        double cgpa = 4.2;
+        char grade = 'E';
+        System.out.println(("Hello " + fullName));
+        System.out.println("You are " + age + " years old");
+        System.out.println("Your cgpa is: " + cgpa);
+        System.out.println("Your average letter grade is: " + grade);
 
     }
 }
