7a31e27f5906ab2bb185826edcc4ad14eb9a9498
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-27 13:22:49 +0530

    Adding code for first java code in a long time in java 24

--- src/FirstCodeOnJavaLatest.java
+++ src/FirstCodeOnJavaLatest.java
@@ -1,7 +1,19 @@
+/**
+ * My first Java refreshment code for learning Java 24
+ */
 public class FirstCodeOnJavaLatest {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java 24");
-        System.out.println("Refreshing my Java knowledge");
-        System.out.print("Lets see how long it takes for me to complete this course");
+        System.out.print("Welcome to Java 24\n");
+        System.out.print("Refreshing my Java knowledge. ");
+        System.out.println("Lets see how long it takes for me to complete this course");
+        // sout + tab: shortcut to write "System.out.println" in intellij
+
+        // Matthew 22:37-40 NLT
+        System.out.println("\nMatthew 22:37-40 NLT:");
+        System.out.println("Jesus replied, “‘You must love the Lord your God with");
+        System.out.println("all your heart, all your soul, and all your mind.’");
+        System.out.println("This is the first and greatest commandment. A second is equally important:");
+        System.out.println("‘Love your neighbor as yourself.’");
+        System.out.println("The entire law and all the demands of the prophets are based on these two commandments.”");
     }
 }
