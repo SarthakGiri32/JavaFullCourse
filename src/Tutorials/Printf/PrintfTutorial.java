a192e426b4f830f36a5bacde56a9452b3a95b606
Sarthak Giri <giri.sarthak1@gmail.com>
2026-01-21 09:16:13 +0530

    Working on printf tutorial section

--- src/Tutorials/Printf/PrintfTutorial.java
+++ src/Tutorials/Printf/PrintfTutorial.java
@@ -6,20 +6,20 @@ public class PrintfTutorial {
         // printf() - method to add special formatting to outputs
         // %[flags][width][.precision][specifier-character]
 
-        String name = "Harry";
-        char firstLetter = 'H';
-        int age = 35;
-        double height = 181.3;
-        boolean isEmployed = false;
+        // String name = "Harry";
+        // char firstLetter = 'H';
+        // int age = 35;
+        // double height = 181.3;
+        // boolean isEmployed = false;
 
         // 1. %[specifier-character]
-        System.out.printf("Hello %s\n", name); // %s: string
-        System.out.printf("Your name starts with a '%c'\n", firstLetter); // %c: character
-        System.out.printf("You are %d years old\n", age); // %d: integer
-        System.out.printf("You are %f centimeters tall\n", height); // %f: double/float
-        System.out.printf("Are you employed?: %b\n", isEmployed); // %b: boolean
+        // System.out.printf("Hello %s\n", name); // %s: string
+        // System.out.printf("Your name starts with a '%c'\n", firstLetter); // %c: character
+        // System.out.printf("You are %d years old\n", age); // %d: integer
+        // System.out.printf("You are %f centimeters tall\n", height); // %f: double/float
+        // System.out.printf("Are you employed?: %b\n", isEmployed); // %b: boolean
 
-        System.out.printf("%s is %d years old", name, age); // can use multiple format specifiers at the same time
+        // System.out.printf("%s is %d years old", name, age); // can use multiple format specifiers at the same time
 
         
 
