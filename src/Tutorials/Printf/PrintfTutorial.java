62aa9f331b3114a52336903a18944a7d08ab347a
Sarthak Giri <giri.sarthak1@gmail.com>
2026-01-21 21:23:20 +0530

    Completing the printf tutorial section

--- src/Tutorials/Printf/PrintfTutorial.java
+++ src/Tutorials/Printf/PrintfTutorial.java
@@ -21,7 +21,92 @@ public class PrintfTutorial {
 
         // System.out.printf("%s is %d years old", name, age); // can use multiple format specifiers at the same time
 
-        
+        // 2. %[.precision]f
+        // double price1 = 9.99;
+        // double price2 = 113.54;
+        // double price3 = -43.87;
+
+        /*
+        without providing any precision value, zeroes are padded to the end till 6 decimal places 
+        (if less than 6 floating point digits are present)
+        */
+        // System.out.printf("%.1f\n", price1);
+        // System.out.printf("%.3f\n", price2);
+        // System.out.printf("%.2f\n", price3);
+
+        // 3. %[flags]
+        // double price1 = 9.99;
+        // double price2 = 113.54;
+        // double price3 = -43.87;
+
+        // a) + = output a plus sign for positive values
+
+        // System.out.printf("%+.2f\n", price1);
+        // System.out.printf("%+.2f\n", price2);
+        // System.out.printf("%+.2f\n", price3);
+
+        // b) , = a comma grouping separator for large values (for thousand, million, billion, etc)
+
+        // price1 = 9000.99;
+        // price2 = 113000.54;
+        // price3 = -4300000000000.87;
+
+        // System.out.printf("%,.2f\n", price1);
+        // System.out.printf("%,.2f\n", price2);
+        // System.out.printf("%,.2f\n", price3);
+
+        // c) ( = negative numbers are enclosed in parenthesis
+
+        // System.out.printf("%(.2f\n", price1);
+        // System.out.printf("%(.2f\n", price2);
+        // System.out.printf("%(.2f\n", price3);
+
+        // System.out.printf("%(+.2f\n", price1);
+        // System.out.printf("%(+.2f\n", price2);
+        // System.out.printf("%(+.2f\n", price3); // the negative sign is overridden by the parenthesis
+
+        // d) whitespace = whitespace if positive value and minus sign if negative value
+
+        // System.out.printf("% .2f\n", price1);
+        // System.out.printf("% .2f\n", price2);
+        // System.out.printf("% .2f\n", price3);
+
+        // System.out.printf("%( .2f\n", price1);
+        // System.out.printf("%( .2f\n", price2);
+        // System.out.printf("%( .2f\n", price3);
+
+        // System.out.printf("%(+.2f\n", price1);
+        // System.out.printf("%(+.2f\n", price2);
+        // System.out.printf("%(+.2f\n", price3);
+
+        // 4. [width]
+        int id1 = 1;
+        int id2 = 45;
+        int id3 = 671;
+        int id4 = 8234;
+        // we can align these id values by setting a specific width in printf
+
+        // 0 : zero padding
+        // positive integer: right justified padding
+        // negative integer: left justified padding
+
+        // right justified
+        System.out.printf("%9d\n", id1);
+        System.out.printf("%9d\n", id2);
+        System.out.printf("%9d\n", id3);
+        System.out.printf("%9d\n", id4); 
+
+        // left justified
+        System.out.printf("%-5d\n", id1);
+        System.out.printf("%-5d\n", id2);
+        System.out.printf("%-5d\n", id3);
+        System.out.printf("%-5d\n", id4);
+
+        // zero padding (right justified)
+        System.out.printf("%09d\n", id1);
+        System.out.printf("%09d\n", id2);
+        System.out.printf("%09d\n", id3);
+        System.out.printf("%09d\n", id4); 
 
     }
 }
