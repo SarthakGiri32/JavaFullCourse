b037178e90cf4a5fdca4ecfb27d929f7c9d28f56
Sarthak Giri <giri.sarthak1@gmail.com>
2026-01-27 20:31:15 +0530

    Completing for loops tutorial section

---
+++ src/main/java/Exercises/ForLoops/Exercise1.java
@@ -0,0 +1,28 @@
+package Exercises.ForLoops;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) throws InterruptedException {
+        
+        // countdown simulator
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the seconds to countdown: ");
+        int start = scanner.nextInt();
+
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+            /* 
+            Causes the currently executing thread to sleep (temporarily cease execution) for the specified number of milliseconds, 
+            subject to the precision and accuracy of system timers and schedulers. The thread does not lose ownership of any monitors.
+            */
+        }
+
+        System.out.println("Happy New Year! 🎉🎉🎊🎊");
+        
+        scanner.close();
+
+    }
+}
---
+++ src/main/java/Tutorials/ForLoops/ForLoopsTutorial.java
@@ -0,0 +1,42 @@
+package Tutorials.ForLoops;
+
+import java.util.Scanner;
+
+public class ForLoopsTutorial {
+    public static void main(String[] args) {
+        
+        // for (int i = 1; i <= 10; i++) {
+        //     // System.out.println("Chicken wings");
+        //     System.out.println(i);
+        // }
+        // executes any code in its body for a definite amount of time (depending on the loop counter variable 'i')
+
+        // 1. decrementing i through for loop
+        // for (int i = 10; i >= 1; i--) {
+        //     System.out.println(i);
+        // }
+
+        // 2. incrementing i by 3
+        // for (int i = 1; i <= 10; i += 3) {
+        //     System.out.println(i);
+        // }
+
+        // 3. decrementing by 2
+        // for (int i = 10; i >= 1; i -= 2) {
+        //     System.out.println(i);
+        // }
+
+        // 4.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the loop count: ");
+        int max = scanner.nextInt();
+
+        for (int i = 1; i <= max; i++) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+
+    }
+}
