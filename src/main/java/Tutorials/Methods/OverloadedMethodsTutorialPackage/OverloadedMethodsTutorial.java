7f79ae6eb8141e2b540aacf66f0fbaf52d25782b
Sarthak Giri <giri.sarthak1@gmail.com>
2026-01-28 21:57:46 +0530

    Working on overloaded methods tutorial section

---
+++ src/main/java/Tutorials/Methods/OverloadedMethodsTutorialPackage/OverloadedMethodsTutorial.java
@@ -0,0 +1,39 @@
+package Tutorials.Methods.OverloadedMethodsTutorialPackage;
+
+public class OverloadedMethodsTutorial {
+    public static void main(String[] args) {
+        
+        // overloaded methods - methods that share the same name but different parameters
+
+        // System.out.println(add(34, 20, 56));
+        /* The method add(double, double) in the type OverloadedMethodsTutorial is not applicable for the arguments (int, int, int) */
+
+        // System.out.println(add(54, 51));
+        // System.out.println(add(31, 2, 92));
+        // method overloading, since there are two versions of the 'add()' method having different parameters
+        // System.out.println(add(51, 52, 87, 7));
+
+        System.out.println(bakePizza("Whole bread"));
+
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    // static double add(double a, double b) {
+    //     return a + b + c;
+    // } // Duplicate method add(double, double) in type OverloadedMethodsTutorial
+
+    static double add(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    static double add(double a, double b, double c, double d) {
+        return a + b + c + d;
+    }
+
+    static String bakePizza(String bread) {
+        return bread + " pizza";
+    }
+}
