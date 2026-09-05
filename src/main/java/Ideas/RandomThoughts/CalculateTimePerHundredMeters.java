d58721a4887b3a5f009e8c9f0a272a0cffe85bcb
Sarthak Giri <giri.sarthak1@gmail.com>
2026-09-05 16:37:09 +0530

    Adding example data to test time-per-hundred-meters calculation code

--- src/main/java/Ideas/RandomThoughts/CalculateTimePerHundredMeters.java
+++ src/main/java/Ideas/RandomThoughts/CalculateTimePerHundredMeters.java
@@ -17,7 +17,7 @@ public class CalculateTimePerHundredMeters {
     private CalculateTimePerHundredMeters() throws JacksonException {
         ObjectMapper distanceAndTimeDataObjectMapper = new ObjectMapper();
         distanceAndTimeData = distanceAndTimeDataObjectMapper.readValue(
-                new File("src/main/resources/Distance_and_Time_Data_7.json"), DistanceAndTimeData.class);
+                new File("src/main/resources/DistanceAndTimeDataExample.json"), DistanceAndTimeData.class);
     }
 
     private List<Float> getSplitDistanceArray() {
